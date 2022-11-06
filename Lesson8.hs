073f17c72d3ca99e7ae0874648c663616d489d93
DanE4 <erosdani53@gmail.com>
2022-11-06 18:19:48 +0100

    .

--- Lesson8.hs
+++ Lesson8.hs
@@ -16,9 +16,7 @@ module Lesson8 where
     differences (x:xs) = zipWith (-) xs (x:xs)
     -- == [2,3,4,5] [1,2,3,4,5] -> 2 - 1 : 3 - 2...
     --magasabbrendű függvények valszeg + függvénykompozíció ON ZHHHHHHHHHH
-
-
-
+    
 ---------------------------------------------------------------------------------
     group :: Eq a => [a]{-véges-} -> [[a]]
     group [] = []
@@ -41,7 +39,7 @@ module Lesson8 where
     függvénykompozíció
      :t (.)
     (3+) ((2*) 5)
-    függvénykommpal: 
+    függvénykommppal: 
     
     --}
 ---------------------------------------------------------------------------------    
